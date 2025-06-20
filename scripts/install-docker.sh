0bc88219bd19fbe1e196b662e26cebcf91e04a4f
luxece <luxece.com@gmail.com>
2025-06-21 00:15:56 +0700

    sudo

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Cập nhật hệ thống
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 # Cài gói hỗ trợ HTTPS cho apt
-sudo apt install -y ca-certificates curl gnupg lsb-release
+apt install -y ca-certificates curl gnupg lsb-release
 
 # Thêm kho Docker chính thức
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg]  https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg]  https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Cài Docker Engine
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Chạy Docker không cần sudo
-sudo usermod -aG docker $USER
+# Chạy Docker không cần usermod -aG docker $USER
 newgrp docker
